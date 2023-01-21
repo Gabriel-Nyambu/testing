228ef8447e7da626006dc29fe09e405ba718f1fd
Gabriel Nyambu Mwaruta <gabrielnyambu59@gmail.com>
2023-01-22 02:41:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document
+  .querySelectorAll(".about .video-container .controls .control-btn")
+  .forEach((btn) => {
+    btn.onclick = () => {
+      let src = btn.getAttribute("data-src");
+      document.querySelector(".about .video-container .video").src = src;
+    };
+  });
+
+//........read more...........................//
+
+const parentContainer = document.querySelector(".box-container");
+parentContainer.addEventListener("click", (event) => {
+  const current = event.target;
+  const isReadMoreBtn = current.className.includes("read-more-btn");
+  if (!isReadMoreBtn) return;
+  const currentText = event.target.parentNode.querySelector(".read-more-text");
+  currentText.classList.toggle("read-more-text--show");
+  current.textContent = current.textContent.includes("read more")
+    ? "read less >"
+    : "read more >";
+});
